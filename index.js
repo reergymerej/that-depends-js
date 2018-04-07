906329ca99fee414e85610958bb5c6df90fa7b66
Jeremy Greer <jeremy.greer.atl@gmail.com>
2018-04-07 18:41:35 -0400

    Find imports/requires

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-module.exports = () => {
-  return 'x'
+const findImports = (input) => {
+  const regex = (/from\s+['"](.+)['"]/g)
+  const allMatches = []
+  let match
+  while (match = regex.exec(input)) {
+    allMatches.push(match[1])
+  }
+  return allMatches
+}
+
+const findRequires = (input) => {
+  const regex = (/require\(['"](.+)['"]\)/g)
+  const allMatches = []
+  let match
+  while (match = regex.exec(input)) {
+    allMatches.push(match[1])
+  }
+  return allMatches
+}
+
+module.exports = (input) => {
+  const allMatches = [
+    ...findImports(input),
+    ...findRequires(input),
+  ]
+  return allMatches.sort()
 }
--- index.test.js
+++ index.test.js
@@ -1,17 +1,74 @@
 const app = require('./')
 
-describe('import', () => {
-  it('should find one', () => {
-    const input = 'z'
+describe('default export', () => {
+  it('it should find nothing', () => {
+    const input = `console.log('yo')`
     const result = app(input)
-    expect(result).toBe('x')
+    expect(result).toEqual([])
   })
 
-  it('should find many')
-})
+  it('should find one', () => {
+    const input = `import x from 'x'`
+    const result = app(input)
+    expect(result).toEqual(['x'])
+  })
 
-describe('require', () => {
-  it('should find one')
+  it('should find many', () => {
+    const input = `import x from 'x'
+        import x from 'y'
+      `
+    const result = app(input)
+    expect(result).toEqual([
+      'x',
+      'y',
+    ])
+  })
 
-  it('should find many')
+  it('should not depdupe', () => {
+    const input = `import x from 'x'
+        import x from 'y'
+        import x from 'y'
+        `
+    const result = app(input)
+    expect(result).toEqual([
+      'x',
+      'y',
+      'y',
+    ])
+  })
+
+  it('should sort them for consistency', () => {
+    const input = `import x from 'z'
+        import x from 'a'
+        import x from './a'
+        `
+    const result = app(input)
+    expect(result).toEqual([
+      './a',
+      'a',
+      'z',
+    ])
+  })
+
+  it('should work for double quotes', () => {
+    const input = 'import x from "x"'
+    const result = app(input)
+    expect(result).toEqual(['x'])
+  })
+
+  it('should work for require', () => {
+    const input = `import x from 'z'
+        const blah = require('../../blah')
+        import x from './a'
+        `
+    const result = app(input)
+    expect(result).toEqual([
+      '../../blah',
+      './a',
+      'z',
+    ])
+  })
+
+  it('should handle //')
+  it('should handle /* */')
 })
